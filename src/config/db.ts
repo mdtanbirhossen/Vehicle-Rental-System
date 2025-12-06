786f0372c6dca087fb4a72349c084ce8077483f2
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-12-06 16:07:49 +0600

    feat: added bookings and vehicles table

--- src/config/db.ts
+++ src/config/db.ts
@@ -8,15 +8,38 @@ export const pool = new Pool({
 
 const initDB = async () => {
   await pool.query(`
-        CREATE TABLE IF NOT EXISTS users(
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(150) UNIQUE NOT NULL CHECK (email = LOWER(email)),
-        password TEXT NOT NULL CHECK (LENGTH(password) >= 6),
-        phone VARCHAR(15),
-        role VARCHAR(50) NOT NULL CHECK (role IN ('admin', 'customer'))
-        )
-        `);
+    CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL CHECK (email = LOWER(email)),
+    password TEXT NOT NULL CHECK (LENGTH(password) >= 6),
+    phone VARCHAR(15) NOT NULL,
+    role VARCHAR(50) NOT NULL CHECK (role IN ('admin', 'customer'))
+    )
+    `);
+
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS vehicles(
+    id SERIAL PRIMARY KEY,
+    vehicle_name VARCHAR(100) NOT NULL,
+    type VARCHAR(50) NOT NULL CHECK (type IN ('car', 'bike', 'van', 'SUV')),
+    registration_number VARCHAR(50) UNIQUE NOT NULL,
+    daily_rent_price INT NOT NULL CHECK (daily_rent_price > 0),
+    availability_status VARCHAR(50) NOT NULL CHECK (availability_status IN ('available', 'booked'))
+    )
+    `);
+
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS bookings(
+    id SERIAL PRIMARY KEY,
+    customer_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    vehicle_id INTEGER NOT NULL REFERENCES vehicles(id) ON DELETE CASCADE,
+    rent_start_date DATE NOT NULL,
+    rent_end_date DATE NOT NULL,
+    total_price INT NOT NULL CHECK (total_price >= 0),
+    status VARCHAR(50) NOT NULL CHECK (status IN ('active', 'cancelled', 'returned'))
+    )
+    `);
 };
 
 export default initDB;
