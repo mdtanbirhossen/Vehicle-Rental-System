74a1157abac2170ee7f94ec2d825742eea27c0b7
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-12-07 14:51:13 +0600

    feat: auto return interval functionality implemented

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
 import app from "./app";
 import config from "./config";
-
+import {autoReturnBookings} from "./modules/booking/booking.service"
 const port = config.port;
 
 app.listen(port, () => {
   console.log(`Vehicle Rental System app listening on port ${port}`);
 });
+
+
+// Auto-return bookings
+setInterval(async () => {
+  try {
+    console.log("Running auto return job...");
+    await autoReturnBookings();
+  } catch (error) {
+    console.error("Auto return job failed:", error);
+  }
+}, 60 * 1000); 
